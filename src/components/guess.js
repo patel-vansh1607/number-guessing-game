a816f5ba36f8da4e0c449bbe414cc8b0fb4ea5c3
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-23 11:30:03 +0300

    adding the skeleton component

---
+++ src/components/guess.js
@@ -0,0 +1,13 @@
+import "../styles/guess.css"
+
+const NumberGuessingGame = () => {
+    return(
+        <div className="main-div">
+            <div className="main">
+                <h1>Number Guessing Game</h1>
+            </div>
+        </div>
+    )
+}
+
+export default NumberGuessingGame
