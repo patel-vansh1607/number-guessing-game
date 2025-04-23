a710b275cc9f014fa0c3f1ee893aefc5d8c5f8dd
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-23 11:31:01 +0300

    adding the buttons

--- src/components/guess.js
+++ src/components/guess.js
@@ -5,6 +5,10 @@ const NumberGuessingGame = () => {
         <div className="main-div">
             <div className="main">
                 <h1>Number Guessing Game</h1>
+                <form onSubmit={handleSubmit}>
+                <input type="number" value={guess} onChange={handleGuessChange} placeholder="Enter a number between 1 and 100"/>
+                <button type="submit">Submit Guess</button>
+                </form>
             </div>
         </div>
     )
